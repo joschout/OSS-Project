739389a4db65a882d4037789b56f4133e7442cb5
evanermen <elinevanermen@gmail.com>
2014-11-25 16:36:32 +0100

    scatter af?

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/ScatterPlot.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/ScatterPlot.java
@@ -3,8 +3,11 @@ package be.kuleuven.cs.oss.charts;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
+import be.kuleuven.cs.oss.datautils.Position;
 import be.kuleuven.cs.oss.drawingPackage.IDraw;
 import be.kuleuven.cs.oss.resourceproperties.ResourceProperty;
+import be.kuleuven.cs.oss.resourcevisualizations.Box;
+import be.kuleuven.cs.oss.resourcevisualizations.BoxResourceVisualizationFactory;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualization;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualizationFactory;
 import be.kuleuven.cs.oss.sonarfacade.Resource;
@@ -13,7 +16,7 @@ import be.kuleuven.cs.oss.sonarfacade.SonarFacade;
 public class ScatterPlot extends Chart{
 
 	public ScatterPlot(List<Resource> resources, ResourceVisualizationFactory RVF, List<ResourceProperty> resourceProps, SonarFacade sonarF) {
-		super(resources, RVF, resourceProps, sonarF);
+		super(resources, RVF, sonarF);
 	}
 
 	@Override
@@ -21,13 +24,19 @@ public class ScatterPlot extends Chart{
 		fillResourceVisualizations;
 		for(ResourceVisualization rv: RVs){
 			rv.draw(IDraw);
+			IDraw.getBufferedImage();
 		}
 			
 		return null;
 	}
 	
 	private void fillResourceVisualizations(){
-		
+		for(Resource resource: resources){
+			Map<string, Double> properties = super.getResourcePropertyValues(resource);
+			Position position = new Position(properties.get("xcoordinate"), properties.get("ycoordinate"));
+			Box box = BoxResourceVisualizationFactory.create(position, properties.get("width"), properties.get("height"), properties.get("color"));
+			this.rvs.add(box);
+		}
 	}
 
 }
