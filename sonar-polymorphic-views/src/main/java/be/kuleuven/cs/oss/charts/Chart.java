c62b3321871f69b6f568c4852ab975ba8e16b14b
Milan van der Meer <milanvdm@hotmail.com>
2014-11-25 15:20:56 +0100

    Chart changes

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/Chart.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/Chart.java
@@ -4,22 +4,29 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 
 import be.kuleuven.cs.oss.drawingPackage.IDraw;
+import be.kuleuven.cs.oss.resourceproperties.ResourceProperty;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualization;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualizationFactory;
 import be.kuleuven.cs.oss.sonarfacade.Resource;
+import be.kuleuven.cs.oss.sonarfacade.SonarFacade;
 
 public abstract class Chart {
 
 	protected List<ResourceVisualization> RVs;
 	protected List<Resource> resources;
 	protected ResourceVisualizationFactory RVF;
+	protected List<ResourceProperty> resourceProps;
+	protected SonarFacade sonarF;
 	
 	//TODO LineFactory has tob e one of the arguments
-	public Chart(List<Resource> resources, ResourceVisualizationFactory RVF) {
+	public Chart(List<Resource> resources, ResourceVisualizationFactory RVF, List<ResourceProperty> resourceProps, SonarFacade sonarF) {
 		this.resources = resources;
 		this.RVF = RVF;
+		this.resourceProps = resourceProps;
+		this.sonarF = sonarF;
 		
 	}
+	
 	
 	public abstract BufferedImage draw(IDraw drawInterface);
 	
--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/SystemComplexity.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/SystemComplexity.java
@@ -4,13 +4,15 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 
 import be.kuleuven.cs.oss.drawingPackage.IDraw;
+import be.kuleuven.cs.oss.resourceproperties.ResourceProperty;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualizationFactory;
 import be.kuleuven.cs.oss.sonarfacade.Resource;
+import be.kuleuven.cs.oss.sonarfacade.SonarFacade;
 
 public class SystemComplexity extends Chart {
 	
-	public SystemComplexity(List<Resource> resources, ResourceVisualizationFactory RVF) {
-		super(resources, RVF);
+	public SystemComplexity(List<Resource> resources, ResourceVisualizationFactory RVF, List<ResourceProperty> resourceProps, SonarFacade sonarF) {
+		super(resources, RVF, resourceProps, sonarF);
 	}
 
 	@Override
@@ -19,6 +21,6 @@ public class SystemComplexity extends Chart {
 		return null;
 	}
 
-	
+	public 
 	
 }
