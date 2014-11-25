e7213278115ae3f3a3e047c06efa0a6868fd59ee
Milan van der Meer <milanvdm@hotmail.com>
2014-11-25 16:05:28 +0100

    Added ResourcePropertyLogic

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/SystemComplexity.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/SystemComplexity.java
@@ -4,15 +4,14 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 
 import be.kuleuven.cs.oss.drawingPackage.IDraw;
-import be.kuleuven.cs.oss.resourceproperties.ResourceProperty;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualizationFactory;
 import be.kuleuven.cs.oss.sonarfacade.Resource;
 import be.kuleuven.cs.oss.sonarfacade.SonarFacade;
 
 public class SystemComplexity extends Chart {
 	
-	public SystemComplexity(List<Resource> resources, ResourceVisualizationFactory RVF, List<ResourceProperty> resourceProps, SonarFacade sonarF) {
-		super(resources, RVF, resourceProps, sonarF);
+	public SystemComplexity(List<Resource> resources, ResourceVisualizationFactory RVF, SonarFacade sonarF) {
+		super(resources, RVF, sonarF);
 	}
 
 	@Override
@@ -21,6 +20,8 @@ public class SystemComplexity extends Chart {
 		return null;
 	}
 
-	public 
+	public void getResourcePropertyValues(Resource resource) {
+		
+	}
 	
 }
