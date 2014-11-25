e1eba8fb8720fc9e6a23a12e1fdc739fab13afdd
Milan van der Meer <milanvdm@hotmail.com>
2014-11-25 15:12:13 +0100

    Added constructor to Chart

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/Chart.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/Chart.java
@@ -8,11 +8,18 @@ import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualization;
 import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualizationFactory;
 import be.kuleuven.cs.oss.sonarfacade.Resource;
 
-public abstract class IChart {
+public abstract class Chart {
 
 	protected List<ResourceVisualization> RVs;
 	protected List<Resource> resources;
 	protected ResourceVisualizationFactory RVF;
+	
+	//TODO LineFactory has tob e one of the arguments
+	public Chart(List<Resource> resources, ResourceVisualizationFactory RVF) {
+		this.resources = resources;
+		this.RVF = RVF;
+		
+	}
 	
 	public abstract BufferedImage draw(IDraw drawInterface);
 	
--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/SystemComplexity.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/charts/SystemComplexity.java
@@ -1,10 +1,17 @@
 package be.kuleuven.cs.oss.charts;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 import be.kuleuven.cs.oss.drawingPackage.IDraw;
+import be.kuleuven.cs.oss.resourcevisualizations.ResourceVisualizationFactory;
+import be.kuleuven.cs.oss.sonarfacade.Resource;
 
-public class SystemComplexity extends IChart {
+public class SystemComplexity extends Chart {
+	
+	public SystemComplexity(List<Resource> resources, ResourceVisualizationFactory RVF) {
+		super(resources, RVF);
+	}
 
 	@Override
 	public BufferedImage draw(IDraw drawInterface) {
