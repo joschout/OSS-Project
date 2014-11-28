41e72550470f4b4af66f75bbe5b04372b0d69d73
Jerre1990 <jeroen.reinenbergh@gmail.com>
2014-11-28 02:13:45 +0100

    Minor changes to Controller

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/control/Controller.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/control/Controller.java
@@ -68,7 +68,7 @@ public class Controller {
 	 * Create a new visualization factory (currently, only boxes are supported)
 	 * @return a new visualization factory
 	 */
-	public ResourceVisualizationFactory createRVFactory(){
+	private ResourceVisualizationFactory createRVFactory(){
 		LOG.info("create RVF");
 		return new BoxFactory();
 	}
@@ -77,7 +77,7 @@ public class Controller {
 	 * Create a new line factory (currently, only straight lines are supported)
 	 * @return a new line factory
 	 */
-	public LineFactory createLineFactory(){
+	private LineFactory createLineFactory(){
 		LOG.info("create LF");
 		return new StraightLineFactory();
 	}
