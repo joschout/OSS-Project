352987d971ac4d42be823a2f19f5987998a09161
Jerre1990 <jeroen.reinenbergh@gmail.com>
2014-12-14 17:35:57 +0100

    Started on ShapeDeciderHandler

---
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/control/ShapeDeciderHandler.java
@@ -0,0 +1,21 @@
+package be.kuleuven.cs.oss.control;
+
+import org.sonar.api.charts.ChartParameters;
+
+import be.kuleuven.cs.oss.charts.Chart;
+
+public class ShapeDeciderHandler implements IHandler<Chart>{
+
+	@Override
+	public void setNext(IHandler<Chart> handler) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleRequest(Chart mutableObject, ChartParameters params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
