a8bd78ae90635eebdb49d54346da8b73d5a2fdb9
evanermen <elinevanermen@gmail.com>
2014-12-16 19:41:35 +0100

    nog eens scattertest

--- sonar-polymorphic-views/src/test/java/be/kuleuven/cs/oss/charts/ScatterPlotTest2.java
+++ sonar-polymorphic-views/src/test/java/be/kuleuven/cs/oss/charts/ScatterPlotTest2.java
@@ -36,6 +36,7 @@ public class ScatterPlotTest2 {
 		scatterplot.setMaxResourceSize(new Size(0,0));
 		scatterplot.setMinResourceSize(new Size(Integer.MAX_VALUE, Integer.MAX_VALUE));
 
+		//change the 5 to 6 to get different result (originbox)
 		Resource res = mock(Resource.class);
 		ArrayList<Resource> reslist = new ArrayList<Resource>();
 		for(int i=1; i<5; i++){
@@ -90,7 +91,7 @@ public class ScatterPlotTest2 {
 		Box box2 = new Box(new Position(0,0), new Size(80,10), new Color(50,50,50), "Big");
 		Box box3 = new Box(new Position(0,0), new Size(50,50), new Color(100,100,100), "Bigass");
 		Box box4 = new Box(new Position(0,0), new Size(15,30), new Color(150,150,150), "Medium");
-		Box box5 = new Box(new Position(0,0), new Size(5,5), new Color(150,150,150), "oorsprong");
+		Box box5 = new Box(new Position(0,0), new Size(5,5), new Color(150,150,150), "origin");
 
 		scatterplot.setRvf(rvc);
 		when(rvc.create(any(Resource.class))).thenReturn(box1).thenReturn(box2).thenReturn(box3).thenReturn(box4).thenReturn(box5);
@@ -118,11 +119,11 @@ public class ScatterPlotTest2 {
 	@Test
 	public void giantResourceTest() {
 		
-		Box box1 = new Box(new Position(0,0), new Size(5,5), new Color(10,10,10), "VerySmall");
-		Box box2 = new Box(new Position(0,0), new Size(20,10), new Color(50,50,50), "Big");
-		Box box3 = new Box(new Position(0,0), new Size(5,20), new Color(100,100,100), "Bigass");
-		Box box4 = new Box(new Position(0,0), new Size(800,700), new Color(150,150,150), "Medium");
-		Box box5 = new Box(new Position(0,0), new Size(0,0), new Color(150,150,150), "oorsprong");
+		Box box1 = new Box(new Position(0,0), new Size(5,5), new Color(10,10,10), "little box");
+		Box box2 = new Box(new Position(0,0), new Size(20,10), new Color(50,50,50), "box2");
+		Box box3 = new Box(new Position(0,0), new Size(5,20), new Color(100,100,100), "box3");
+		Box box4 = new Box(new Position(0,0), new Size(9000,800), new Color(150,150,150), "Giantass");
+		Box box5 = new Box(new Position(0,0), new Size(0,0), new Color(150,150,150), "origin");
 
 		scatterplot.setRvf(rvc);
 		when(rvc.create(any(Resource.class))).thenReturn(box1).thenReturn(box2).thenReturn(box3).thenReturn(box4).thenReturn(box5);
