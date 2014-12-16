b352a347e495fbfaec231e5f932ab050ae6ecbfe
LennDG <lennart.degraef@student.kuleuven.be>
2014-12-16 11:04:27 +0100

    ShapeDeciders are workiiing

--- sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/control/ResourceVisualizationCreatorHandler.java
+++ sonar-polymorphic-views/src/main/java/be/kuleuven/cs/oss/control/ResourceVisualizationCreatorHandler.java
@@ -107,13 +107,20 @@ public class ResourceVisualizationCreatorHandler implements IHandler<Chart>{
 		
 		List<Integer> boundaries = parseStringListToIntList(parseStringList(split,"x"));
 		boundaries.add(Integer.MAX_VALUE);
-		
+		try{
 		if(shapes.size() == boundaries.size()){
 			for(String shape : shapes){
+				System.out.println(shape);
+				System.out.println(boundaries.get(shapes.indexOf(shape)));
 				sd.addBoundaryWithFactory(boundaries.get(shapes.indexOf(shape)),createRVF(shape,params));
 			}
 		}
+		
 		else throw new IllegalArgumentException("ShapeMetricOrder and ShapeMetricSplit combination not valid");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	private String retrieveValue(String key, ChartParameters params) {
